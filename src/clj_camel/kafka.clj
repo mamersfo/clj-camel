76a893c6f476bbe68e6809ad78ab280abcd79e2f
Martin van Amersfoorth <mamersfo@gmail.com>
2015-04-21 07:58:57 +0200

    Typos

--- src/clj_camel/kafka.clj
+++ src/clj_camel/kafka.clj
@@ -1,17 +1,16 @@
-(ns clj-camel.example
+(ns clj-camel.kafka
   (:require [clj-camel.core :as camel :refer [defroute]]))
 
-(defroute kafka
+(defroute kafka-route
   (.from (str "kafka:192.168.59.103:9092?"
               "topic=test&"
               "zookeeperHost=192.168.59.103&"
               "zookeeperPort=2181&"
               "groupId=camel"))
-  ;; (.to "log:clj-camel.example?level=INFO&showAll=true&multiline=true")
   (.to "hdfs2://localhost:9000/tmp/foo?splitStrategy=IDLE:5000"))
 
 (defn run
   []
-  (let [ctx (camel/start kafka)]
+  (let [ctx (camel/start kafka-route)]
     (Thread/sleep 60000)
     (camel/shutdown ctx)))
